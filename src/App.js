f54abf4d60ec18a69aebdc317a963bd7b5c534b8
aidanbell0 <aidanbell0182@gmail.com>
2020-02-05 17:34:57 -0500

    Moved to React

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      width: 1,
+      height: 1,
+      count: 1
+    }
+  }
+
+  handleChange(evt) {
+    if (evt.target.name === "height") { this.setState({ height: parseInt(evt.target.value) })};
+    if (evt.target.name === "width") { this.setState({ width: parseInt(evt.target.value) })};
+    if (evt.target.name === "count") { this.setState({ count: parseInt(evt.target.value) })};
+  }
+
+  handleSubmit(evt) {
+    if (evt.target.name === "submit") {
+
+    }
+  }
+
+  draw() {
+    for (let i = 0; i > this.state.count; i++) {
+      console.log("ding")
+      return (<div id='tile'></div>)
+    }
+  }
+
+  render() {
+    console.log("rendered")
+    return (
+      <div className="App">
+        <div id="dungeon_input">
+          <label for="height">Enter desired height:</label>
+          <input type="number" name="height" id="height_input" onChange={this.handleChange.bind(this)} value={this.state.height} />
+          <label for="width">Enter desired width:</label>
+          <input type="number" name="width" id="width_input" onChange={this.handleChange.bind(this)} value={this.state.width} />
+            <label for="width">Enter desired tile count:</label>
+            <input type="number" name="count" id="count_input" onChange={this.handleChange.bind(this)} value={this.state.count} />
+          <button type="submit" name="submit">Submit</button>
+        </div>
+        <div id="dungeon_output">
+          {this.draw}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
