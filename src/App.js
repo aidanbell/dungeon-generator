dac5b8c6d0c8f4ecd48965b9f9f48dd2c8968a33
aidanbell0 <aidanbell0182@gmail.com>
2020-02-06 10:19:44 -0500

    Grid is rendering

--- src/App.js
+++ src/App.js
@@ -2,18 +2,25 @@ import React, { Component } from 'react';
 
 import './App.css';
 
+let column = [];
+let row = [];
+
 class App extends Component {
   constructor() {
     super();
     this.state = {
       width: 1,
       height: 1,
-      count: 1
+      count: 1,
     }
   }
 
   handleChange(evt) {
-    if (evt.target.name === "height") { this.setState({ height: parseInt(evt.target.value) })};
+    if (evt.target.name === "height") {
+      this.setState({
+        height: parseInt(evt.target.value),
+      })
+    };
     if (evt.target.name === "width") { this.setState({ width: parseInt(evt.target.value) })};
     if (evt.target.name === "count") { this.setState({ count: parseInt(evt.target.value) })};
   }
@@ -25,10 +32,9 @@ class App extends Component {
   }
 
   draw() {
-    for (let i = 0; i > this.state.count; i++) {
-      console.log("ding")
-      return (<div id='tile'></div>)
-    }
+    column = Array(this.state.height).fill(<div id="tile"></div>);
+    row = Array(this.state.width).fill(<div id="tile"></div>);
+    console.log(column)
   }
 
   render() {
@@ -36,16 +42,20 @@ class App extends Component {
     return (
       <div className="App">
         <div id="dungeon_input">
-          <label for="height">Enter desired height:</label>
+          <label>Enter desired height:</label>
           <input type="number" name="height" id="height_input" onChange={this.handleChange.bind(this)} value={this.state.height} />
-          <label for="width">Enter desired width:</label>
+          <label>Enter desired width:</label>
           <input type="number" name="width" id="width_input" onChange={this.handleChange.bind(this)} value={this.state.width} />
-            <label for="width">Enter desired tile count:</label>
-            <input type="number" name="count" id="count_input" onChange={this.handleChange.bind(this)} value={this.state.count} />
+          <label>Enter desired tile count:</label>
+          <input type="number" name="count" id="count_input" onChange={this.handleChange.bind(this)} value={this.state.count} />
           <button type="submit" name="submit">Submit</button>
         </div>
         <div id="dungeon_output">
-          {this.draw}
+          {this.draw()}
+          {column.map(x => {
+            return <div class="column">{column}</div>
+          })}
+
         </div>
       </div>
     );
